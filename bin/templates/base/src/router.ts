049c1f17ebb8ba5b4ba7bd651f7c3514e1f0728c
Hedzer <hedzer87@gmail.com>
2025-10-05 02:02:52 -0400

    fix: removed transitions

--- bin/templates/base/src/router.ts
+++ bin/templates/base/src/router.ts
@@ -4,13 +4,6 @@ const { page, initialize, navigate } = Router({
   target: '#app',
   type: 'hash',
   layout: 'snice-layout',
-  transition: {
-    mode: 'simultaneous',
-    outDuration: 200,
-    inDuration: 200,
-    out: 'opacity: 0;',
-    in: 'opacity: 1;'
-  }
 });
 
 export { page, initialize, navigate };
