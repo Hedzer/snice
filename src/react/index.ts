473e427972d8d2e88bd34494e8b2637abd52fc24
Hedzer <hedzer87@gmail.com>
2026-03-10 00:57:05 -0400

    feat: add React barrel export

---
+++ src/react/index.ts
@@ -0,0 +1,27 @@
+// Context
+export {
+  SniceProvider,
+  useSniceContext,
+  useNavigate,
+  useParams,
+  useRoute,
+  type SniceReactContext,
+  type SniceProviderProps,
+  type Placard,
+} from './SniceProvider';
+
+// Router
+export {
+  SniceRouter,
+  Route,
+  type SniceRouterProps,
+  type RouteProps,
+} from './SniceRouter';
+
+// Request handler
+export {
+  useRequestHandler,
+  type UseRequestRoute,
+  type UseRequestRouteMap,
+  type UseRequestHandlerOptions,
+} from './useRequestHandler';
